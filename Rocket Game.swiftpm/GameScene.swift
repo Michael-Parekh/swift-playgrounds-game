56fc07f0f58a4cc467996c1ee61bd8b94c858a56
Michael Parekh <mparekh4@illinois.edu>
2024-01-29 16:56:32 -0600

    add rocket controls based on player tap

--- Rocket Game.swiftpm/GameScene.swift
+++ Rocket Game.swiftpm/GameScene.swift
@@ -7,15 +7,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var rocket = SKSpriteNode()
     var rocketSize = CGSize(width: 50, height: 50)
+    var rocketThrustAngularImpulse = 0.005
+    var rocketXThrust = CGFloat(40)
+    var rocketYThrust = CGFloat(115)
+    var rocketCamera = SKCameraNode()
     
     // Add a ground level to prevent the rocket from falling off the screen.
     var groundLevel = SKSpriteNode()
     var groundLevelSize = CGSize(width: 10000, height: 10)
     
+    // Where the user has tapped on the screen - used for determining the rocket movement. 
+    var touchLocation = CGPoint()
+    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         // Reduce gravity slightly for this game. 
         physicsWorld.gravity = CGVector(dx: 0, dy: -0.9)
+        
+        // Keep the camera focused on the rocket as it moves by using SKCameraNode. 
+        self.camera = rocketCamera
         
         rocket = SKSpriteNode(color: .cyan, size: rocketSize)
         // Place the rocket in the middle of the screen. 
@@ -32,5 +42,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // Prevent the 'groundLevel' from moving. 
         groundLevel.physicsBody?.isDynamic = false
         addChild(groundLevel)
+    }
+    
+    // The user can control the rocket by tapping on the screen.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        touchLocation = touch.location(in: self)
+        fireThrusters(location: touchLocation)
+    }
+    
+    func fireThrusters(location: CGPoint) {
+        let adjustedRotation = rocket.zRotation + (CGFloat.pi / 2)
+        let dx = rocketXThrust * cos(adjustedRotation)
+        let dy = rocketYThrust * sin(adjustedRotation)
+        
+        if location.x > 100 + rocket.position.x {
+            // If the player is touching 100 pixels to the right of the rocket, then rotate the rocket to the right. 
+            rocket.physicsBody?.applyAngularImpulse(-rocketThrustAngularImpulse)
+        } else if location.x < -100 + rocket.position.x {
+            // If the player is touching 100 pixels to the left of the rocket, then rotate the rocket to the left. 
+            rocket.physicsBody?.applyAngularImpulse(rocketThrustAngularImpulse)
+        } else {
+            // If the player is touching in between, then move the rocket forward.
+            rocket.physicsBody?.applyImpulse(CGVector(dx: dx, dy: dy))
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        rocketCamera.position = rocket.position
     }
 }
