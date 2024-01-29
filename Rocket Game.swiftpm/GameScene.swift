75cc15af500f4cb1c2afb62dacd35480837c3cea
Michael Parekh <mparekh4@illinois.edu>
2024-01-29 17:36:15 -0600

    add visual tap feedback

--- Rocket Game.swiftpm/GameScene.swift
+++ Rocket Game.swiftpm/GameScene.swift
@@ -19,6 +19,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     // Where the user has tapped on the screen - used for determining the rocket movement. 
     var touchLocation = CGPoint()
     
+    var spinnyNode: SKShapeNode?
+    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         // Reduce gravity slightly for this game. 
@@ -44,6 +46,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // Prevent the 'groundLevel' from moving. 
         groundLevel.physicsBody?.isDynamic = false
         addChild(groundLevel)
+        
+        // Use a 'spinnyNode' to highlight where the player has tapped. 
+        let w = (self.size.width + self.size.height) * 0.05
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5), SKAction.fadeOut(withDuration: 0.5), SKAction.removeFromParent()]))
+        }
     }
     
     // The user can control the rocket by tapping on the screen.
@@ -61,24 +72,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if location.x > 100 + rocket.position.x {
             // If the player is touching 100 pixels to the right of the rocket, then rotate the rocket to the right. 
             rocket.physicsBody?.applyAngularImpulse(-rocketThrustAngularImpulse)
+            
             // Add orange visual thruster feedback to the rocket if turned right. 
             let thrusterParticle = SKSpriteNode(color: .orange, size: CGSize(width: 5, height: 5))
             thrusterParticle.position = rocket.position
             addChild(thrusterParticle)
+            
+            // Add orange visual tap feedback if turned right. 
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = location
+                n.strokeColor = SKColor.orange
+                self.addChild(n)
+            }
         } else if location.x < -100 + rocket.position.x {
             // If the player is touching 100 pixels to the left of the rocket, then rotate the rocket to the left. 
             rocket.physicsBody?.applyAngularImpulse(rocketThrustAngularImpulse)
+            
             // Add red visual thruster feedback to the rocket if turned left.
             let thrusterParticle = SKSpriteNode(color: .red, size: CGSize(width: 5, height: 5))
             thrusterParticle.position = rocket.position
             addChild(thrusterParticle)
+            
+            // Add red visual tap feedback if turned right. 
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = location
+                n.strokeColor = SKColor.red
+                self.addChild(n)
+            }
         } else {
             // If the player is touching in between, then move the rocket forward.
             rocket.physicsBody?.applyImpulse(CGVector(dx: dx, dy: dy))
+            
             // Add blue visual thruster feedback to the rocket if going straight.
             let thrusterParticle = SKSpriteNode(color: .blue, size: CGSize(width: 5, height: 5))
             thrusterParticle.position = rocket.position
             addChild(thrusterParticle)
+            
+            // Add blue visual tap feedback if turned right. 
+            if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+                n.position = location
+                n.strokeColor = SKColor.blue
+                self.addChild(n)
+            }
         }
     }
     
