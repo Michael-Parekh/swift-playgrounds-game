3418be0ad43d5cb5646c63640fecb2d5b3ecabd8
Michael Parekh <mparekh4@illinois.edu>
2024-01-28 23:36:20 -0600

    add physics to rocket

--- Rocket Game.swiftpm/GameScene.swift
+++ Rocket Game.swiftpm/GameScene.swift
@@ -2,13 +2,24 @@ import SwiftUI
 import SpriteKit
 
 // GameScene represents the interactable environment where gameplay occurs. 
-class GameScene: SKScene {
+// SKPhysicsContactDelegate is a protocol used for handling contacts and collisions between physics bodies in a GameScene. 
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var node = SKSpriteNode()
+    var rocket = SKSpriteNode()
+    var rocketSize = CGSize(width: 50, height: 50)
     
     override func didMove(to view: SKView) {
-        node = SKSpriteNode(color: .cyan, size: CGSize(width: 50, height: 50))
+        physicsWorld.contactDelegate = self
+        // Reduce gravity slightly for this game. 
+        physicsWorld.gravity = CGVector(dx: 0, dy: -0.9)
+        
+        rocket = SKSpriteNode(color: .cyan, size: rocketSize)
+        // Place the rocket in the middle of the screen. 
+        rocket.position = CGPoint(x: 300, y: 100)
+        rocket.name = "rocket"
+        rocket.physicsBody = SKPhysicsBody(rectangleOf: rocketSize)
+        
         // Add the node to the GameScene. 
-        addChild(node)
+        addChild(rocket)
     }
 }
