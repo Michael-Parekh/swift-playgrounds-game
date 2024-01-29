b0da95bf8bf3fef3c3a90b9adee51d88a3b46e67
Michael Parekh <mparekh4@illinois.edu>
2024-01-28 23:45:08 -0600

    add ground sprite node to the game

--- Rocket Game.swiftpm/GameScene.swift
+++ Rocket Game.swiftpm/GameScene.swift
@@ -8,6 +8,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var rocket = SKSpriteNode()
     var rocketSize = CGSize(width: 50, height: 50)
     
+    // Add a ground level to prevent the rocket from falling off the screen.
+    var groundLevel = SKSpriteNode()
+    var groundLevelSize = CGSize(width: 10000, height: 10)
+    
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         // Reduce gravity slightly for this game. 
@@ -18,8 +22,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         rocket.position = CGPoint(x: 300, y: 100)
         rocket.name = "rocket"
         rocket.physicsBody = SKPhysicsBody(rectangleOf: rocketSize)
-        
         // Add the node to the GameScene. 
         addChild(rocket)
+        
+        groundLevel = SKSpriteNode(color: .brown, size: groundLevelSize)
+        groundLevel.position = CGPoint(x: 300, y: 10)
+        groundLevel.name = "groundLevel"
+        groundLevel.physicsBody = SKPhysicsBody(rectangleOf: groundLevelSize)
+        // Prevent the 'groundLevel' from moving. 
+        groundLevel.physicsBody?.isDynamic = false
+        addChild(groundLevel)
     }
 }
